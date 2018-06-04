9853d3defa3c7e5389df3a446f3844165b208249
Igor Redchuk <redchuk@gmail.com>
2018-06-04 13:22:44 +0200

    Add supported languages list and reduce results to those languages

--- app/handle-langs-request.js
+++ app/handle-langs-request.js
@@ -1,8 +1,15 @@
+const supportedLangs = require('./supported-langs.json')
+
 async function handleLangsRequest (ctx, fetchAllLangs) {
   try {
-    const result = await fetchAllLangs()
+    const resp = await fetchAllLangs()
+
+    const result = resp.query.languages
+      .filter(l => supportedLangs.includes(l.code))
+      .map(l => ({ lang: l.code, autonym: l['*'] }))
+
     ctx.status = 200
-    ctx.body = result.query.languages
+    ctx.body = result
   } catch (err) {
     ctx.status = 500
     ctx.body = { message: err.toString() }
